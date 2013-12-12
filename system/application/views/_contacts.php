39ffdee0df0b5fa8a285a0033c9e563fecdd4a89
Denis Popov <pod@ciklum.com>
2013-12-12 16:27:19 +0200

    - removed fax - removed phone number - changed phone number - removed the second office - changed title of the first office - changed work time of the first office - changed positions of logos on main page

--- system/application/views/_contacts.php
+++ system/application/views/_contacts.php
@@ -6,167 +6,130 @@
 	hs.graphicsDir = '<?/*=base_url()*/?>js/highslide/graphics/';
 	hs.outlineType = 'rounded-white';
 </script>-->
-<script src="http://api-maps.yandex.ru/1.1/index.xml?key=ADzw600BAAAAnOQ8NQIAxBFbuoPzi14fQXTpnbMudNRqTVEAAAAAAAAAAAA9JEyR6NjLBemwhkInmnzXQBeRQw=="
-        type="text/javascript"></script>
-<script type="text/javascript" src="<?=base_url()?>js/maps.js"></script>
+<script
+    src="http://api-maps.yandex.ru/1.1/index.xml?key=ADzw600BAAAAnOQ8NQIAxBFbuoPzi14fQXTpnbMudNRqTVEAAAAAAAAAAAA9JEyR6NjLBemwhkInmnzXQBeRQw=="
+    type="text/javascript"></script>
+<script type="text/javascript" src="<?= base_url() ?>js/maps.js"></script>
 <?php require_once("_header.php"); ?>
 <script type="text/javascript">
-function action_map(id, action) {
-	if(action == 'show') {
-		$('#'+id).show();
-//		var link = '<a href="#" onclick="javacript:action_map(\''+ id +'\', \'hide\');return false;">Скрыть карту</a>';
-//		$('#link_'+id).html(link);
-	}
-	else if(action == 'hide'){
-		$('#'+id).hide();
-//		var link = '<a href="#" onclick="javacript:action_map(\''+ id +'\', \'show\');return false;">Показать на карте</a>';
-//		$('#link_'+id).html(link);
-	}
-
-	if(id == 'office1') YMaps.load(init1);
-	else if(id == 'office2') YMaps.load(init2);
-
-}
-jQuery(document).ready(function() {
-      action_map('office2', 'show');
-      action_map('office1', 'show');
-});
+    function action_map(id, action)
+    {
+        if (action == 'show') {
+            $('#' + id).show();
+            //		var link = '<a href="#" onclick="javacript:action_map(\''+ id +'\', \'hide\');return false;">Скрыть карту</a>';
+            //		$('#link_'+id).html(link);
+        } else if (action == 'hide') {
+            $('#' + id).hide();
+            //		var link = '<a href="#" onclick="javacript:action_map(\''+ id +'\', \'show\');return false;">Показать на карте</a>';
+            //		$('#link_'+id).html(link);
+        }
+        if (id == 'office1') {
+            YMaps.load(init1);
+        } else if (id == 'office2') {
+            YMaps.load(init2);
+        }
+    }
+    jQuery(document).ready(function ()
+    {
+        action_map('office2', 'show');
+        action_map('office1', 'show');
+    });
 </script>
-	<div class="content">
-		<!-- Содержание -->
-		<?php
-			$address1 = null;
-			$address2 = null;
-			if(!empty($contacts->contact_address)){
-				$contacts->contact_address = unserialize($contacts->contact_address);
-				$address1 = (isset($contacts->contact_address[0]) && empty($contacts->contact_address[0]['contact_address'])) ? null : $contacts->contact_address[0]['contact_address'];
-				$address2 = (isset($contacts->contact_address[1]) && empty($contacts->contact_address[1]['contact_address'])) ? null : $contacts->contact_address[1]['contact_address'];
-			}
-			$map1 = null;
-			$map2 = null;
-			if(!empty($contacts->contact_maps)){
-				$contacts->contact_maps = unserialize($contacts->contact_maps);
-				$map1 = isset($contacts->contact_maps['contact_map1']) ? $contacts->contact_maps['contact_map1'] : null;
-				$map2 = isset($contacts->contact_maps['contact_map2']) ? $contacts->contact_maps['contact_map2'] : null;
-			}
-			$photo1 = null;
-			$photo2 = null;
-			if(!empty($contacts->contact_photos)){
-				$contacts->contact_photos = unserialize($contacts->contact_photos);
-				$photo1 = isset($contacts->contact_photos['contact_photo1']) ? $contacts->contact_photos['contact_photo1'] : null;
-				$photo2 = isset($contacts->contact_photos['contact_photo2']) ? $contacts->contact_photos['contact_photo2'] : null;
-			}
-			$time1 = null;
-			$time2 = null;
-			if(!empty($contacts->contact_times)){
-				$contacts->contact_times = unserialize($contacts->contact_times);
-				$time1 = isset($contacts->contact_times[0]) ? $contacts->contact_times[0] : null;
-				$time2 = isset($contacts->contact_times[1]) ? $contacts->contact_times[1] : null;
-			}
-		?>
-		<div class="about">
-            <div class="data">
-                <h1>Контакты</h1>
-                <!-- Разделитель -->
-                <div class="line_separator">
-                    <div class="line_separator_left">&nbsp;</div>
-                    <div class="line_separator_right">&nbsp;</div>
+<div class="content">
+    <!-- Содержание -->
+    <?php
+        $address1 = null;
+        if (! empty($contacts->contact_address)) {
+            $contacts->contact_address = unserialize($contacts->contact_address);
+            $address1                  = (isset($contacts->contact_address[0]) && empty($contacts->contact_address[0]['contact_address'])) ? null : $contacts->contact_address[0]['contact_address'];
+        }
+        $map1 = null;
+        if (! empty($contacts->contact_maps)) {
+            $contacts->contact_maps = unserialize($contacts->contact_maps);
+            $map1                   = isset($contacts->contact_maps['contact_map1']) ? $contacts->contact_maps['contact_map1'] : null;
+        }
+        $photo1 = null;
+        if (! empty($contacts->contact_photos)) {
+            $contacts->contact_photos = unserialize($contacts->contact_photos);
+            $photo1                   = isset($contacts->contact_photos['contact_photo1']) ? $contacts->contact_photos['contact_photo1'] : null;
+        }
+        $time1 = null;
+        if (! empty($contacts->contact_times)) {
+            $contacts->contact_times = unserialize($contacts->contact_times);
+            $time1                   = isset($contacts->contact_times[0]) ? $contacts->contact_times[0] : null;
+        }
+    ?>
+    <div class="about">
+        <div class="data">
+            <h1>Контакты</h1>
+            <!-- Разделитель -->
+            <div class="line_separator">
+                <div class="line_separator_left">&nbsp;</div>
+                <div class="line_separator_right">&nbsp;</div>
+            </div>
+            <?php if (isset($contacts)){ ?>
+            <div class="part">
+                <div style="float:left; margin-right:20px; margin-top:5px;">
+                    Телефон:
                 </div>
-                <?php if(isset($contacts)){ ?>
-                <div class="part">
-                    <div style="float:left; margin-right:20px; margin-top:9px;">
-                        Телефон:
-                    </div>
-                    <div style="float:left;">
-                    <?php if(!empty($contacts->contact_phones )) : foreach ($contacts->contact_phones as $phone) { ?>
+                <div style="float:left;">
+                    <?php if (! empty($contacts->contact_phones)) : foreach ($contacts->contact_phones as $phone) { ?>
                         <div>
-                        	<p class="phone1" style="font-size:20px;"><?=$phone['contact_kode']?></p>
-                        	<p class="phone2" style="font-size:30px;"><?=$phone['contact_phone']?></p>
+                            <p class="phone1" style="font-size:20px;"><?= $phone['contact_kode'] ?></p>
+
+                            <p class="phone2" style="font-size:30px;"><?= $phone['contact_phone'] ?></p>
                         </div>
                     <?php } endif; ?>
-                    </div>
                 </div>
-                <div class="part">
-                    <div style="float:left; margin-right:20px; margin-top:9px;">
-                        Электронная почта:
-                    </div>
-                    <div style="float:left; margin-top:9px;">
-                    	<?php if(!empty($contacts->contact_emails )) : foreach ($contacts->contact_emails as $email) { ?>
-                        <a href="mailto:<?=$email?>"><?=$email?></a>
-                        <?php } endif; ?>
-                    </div>
+                <div style="float:left; margin-right:20px; margin-top:5px;">
+                    Электронная почта:
                 </div>
-                <div class="part">
-                    <div style="float:left; margin-right:20px; margin-top:9px;">
-                        Факс:
-                    </div>
-                    <div style="float:left;">
-                    <?php if(!empty($contacts->contact_faxes )) : foreach ($contacts->contact_faxes as $fax) { ?>
-                        <div>
-                        	<p class="phone1" style="font-size:30px;"><?=$fax?></p>
-                        </div>
+                <div style="float:left; margin-top:9px;">
+                    <?php if (! empty($contacts->contact_emails)) : foreach ($contacts->contact_emails as $email) { ?>
+                        <a href="mailto:<?= $email ?>"><?= $email ?></a>
                     <?php } endif; ?>
-                    </div>
                 </div>
-                <!-- Разделитель -->
-                <div class="line_separator">
-                    <div class="line_separator_left">&nbsp;</div>
-                    <div class="line_separator_right">&nbsp;</div>
+            </div>
+            <!-- Разделитель -->
+            <div class="line_separator">
+                <div class="line_separator_left">&nbsp;</div>
+                <div class="line_separator_right">&nbsp;</div>
 
-                </div>
-                <div class="part">
-                    <div style="float:left; margin-top:9px;">
-                        <h3>Офис №1:</h3>
-                    </div>
-                </div>
-                <div class="part">
+            </div>
+            <div class="part">
+                <?php
+                    $time_work1 = null;
+                    $timeout1 = null;
 
-                    <div style="float:left; margin-top:9px;">
-                        <h3>Офис №2:</h3>
+                    if ($time1) {
+                        $time_work1 = "с " . $time1['time_from_h'] . ":" . $time1['time_from_m'];
+                        $time_work1 .= " до " . $time1['time_to_h'] . ":" . $time1['time_to_m'];
+                        $timeout2 = "с " . $time1['time_tm_from_h'] . ":" . $time1['time_tm_from_m'];
+                        $timeout2 .= " до " . $time1['time_tm_to_h'] . ":" . $time1['time_tm_to_m'];
+                    }
+                ?>
+                <div style="float:left; margin-top:9px;">
+                    <h3>Офис:</h3>
+                </div>
+                <div class="office_address">
+                    <div class="contact_line"><img src="<?= base_url() . $photo1 ?>"
+                                                   style="max-height:370px;max-width:370px;"></div>
+                    <div class="contact_line"><span>Адрес: </span><span><?= $address1 ?></span></div>
+                    <div class="contact_line"><span>Время работы: </span><span><?= $time_work1 ?></span></div>
+                    <div class="contact_line"><span>Суббота: </span><span>10:00 - 15:00</span></div>
+                    <div class="contact_line"><span>Воскресенье: </span><span>выходной</span></div>
+                    <div>
+                        <div id="office2" style="width:380px;height:400px;display:none;margin-bottom:10px;"></div>
                     </div>
                 </div>
             </div>
-            <?php
-    			$time_work1 = null;
-    			$time_work2 = null;
-    			$timeout1 = null;
-    			$timeout2 = null;
+        </div>
 
-				if($time1) {
-					$time_work1 = "с ".$time1['time_from_h'].":".$time1['time_from_m'];
-					$time_work1 .= " до ".$time1['time_to_h'].":".$time1['time_to_m'];
-					$timeout2 = "с ".$time1['time_tm_from_h'].":".$time1['time_tm_from_m'];
-					$timeout2 .= " до ".$time1['time_tm_to_h'].":".$time1['time_tm_to_m'];
-				}
-				if($time2) {
-					$time_work2 = "с ".$time2['time_from_h'].":".$time2['time_from_m'];
-					$time_work2 .= " до ".$time2['time_to_h'].":".$time2['time_to_m'];
-					$timeout2 = "с ".$time2['time_tm_from_h'].":".$time2['time_tm_from_m'];
-					$timeout2 .= " до ".$time2['time_tm_to_h'].":".$time2['time_tm_to_m'];
-				}
-			?>
-            <div class="floatL data">
-            	<div class="part">
-                    <div class="contact_line"><img src="<?=base_url().$photo1?>" style="max-height:370px;max-width:370px;"></div>
-            		<div class="contact_line"><span>Адрес: </span><span><?=$address1?></span></div>
-            		<div class="contact_line"><span>Время работы: </span><span><?=$time_work1?></span></div>
-            		<div class="contact_line"><span>Перерыв: </span><span><?=$timeout2?></span></div>
-            		<div>
-						<div id="office2" style="width:380px;height:400px;display:none;margin-bottom:10px;"></div>
-            		</div>
-            	</div>
-            	<div class="part">
-                    <div class="contact_line"><img src="<?=base_url().$photo2?>" style="max-height:370px;max-width:370px;"></div>
-            		<div class="contact_line"><span>Адрес: </span><span><?=$address2?></span></div>
-            		<div class="contact_line"><span>Время работы: </span><span><?=$time_work2?></span></div>
-            		<div class="contact_line"><span>Перерыв: </span><span><?=$timeout2?></span></div>
-            		<div>
-						<div id="office1" style="width:380px;height:400px;display:none;margin-bottom:10px;"></div>
-            		</div>
-            	</div>
-            </div>
-            <?php } ?>
-		</div>
-		<div style="clear:both;">&nbsp;</div>
-	</div>
-	<?php require_once('_footer.php'); ?>
+        <div class="floatL data">
+
+        </div>
+        <?php } ?>
+    </div>
+    <div style="clear:both;">&nbsp;</div>
+</div>
+<?php require_once('_footer.php'); ?>
--- system/application/views/_main.php
+++ system/application/views/_main.php
@@ -1,11 +1,11 @@
 <?php mb_internal_encoding("UTF-8"); require_once("_head.php"); ?>
 	<!-- Header implementation -->
-	<?php require_once("_header.php"); ?>	
+	<?php require_once("_header.php"); ?>
 	<!-- Content implementation -->
 	<script type="text/javascript">
 	function strstr (haystack, needle, bool) {
 	    var pos = 0;
-	    
+
 	    haystack += '';
 	    pos = haystack.indexOf( needle );
 	    if (pos == -1) {
@@ -22,20 +22,20 @@
 	$(function($) {
 		var img_path = '';
 		var img_ext = '';
-		
+
 		$("div.categories a img").mouseover(function(){
 			var ext = $(this).attr('src').indexOf(".png");
 			img_path = $(this).attr('src').substr(0, ext);
 			img_ext = $(this).attr('src').substr(ext);
-			
+
 			$(this).attr("src", img_path+'_hover'+img_ext);
-			
+
 		}).mouseout(function(){
 			$(this).attr("src", img_path+img_ext);
 			img_path = '';
 			img_ext = '';
 		});
-	});	
+	});
 	</script>
 	<div class="content">
 		<div>
@@ -44,11 +44,11 @@
 				<?php $about_desc = $about->item_preview; ?>
 				<p><?=$about_desc?></p>
 	            <div class="categories">
-	                <a href="<?php if(!empty($links_cat['doors'])) { echo base_url().'products/category/'.$links_cat['doors'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/doors.png"/></a>
-	                <a href="<?php if(!empty($links_cat['locks'])) { echo base_url().'products/category/'.$links_cat['locks'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/locks.png"/></a>
-	                <a href="<?php if(!empty($links_cat['safes'])) { echo base_url().'products/category/'.$links_cat['safes'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/safes.png"/></a>
+                  <a href="<?php if(!empty($links_cat['safes'])) { echo base_url().'products/category/'.$links_cat['safes'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/safes.png"/></a>
+                  <a href="<?php if(!empty($links_cat['locks'])) { echo base_url().'products/category/'.$links_cat['locks'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/locks.png"/></a>
+                  <a href="<?php if(!empty($links_cat['cylinders'])) { echo base_url().'products/category/'.$links_cat['cylinders'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/cylinders.png"/></a>
+                  <a href="<?php if(!empty($links_cat['doors'])) { echo base_url().'products/category/'.$links_cat['doors'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/doors.png"/></a>
 	                <a href="<?php if(!empty($links_cat['skd'])) { echo base_url().'products/category/'.$links_cat['skd'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/skd.png"/></a>
-	                <a href="<?php if(!empty($links_cat['cylinders'])) { echo base_url().'products/category/'.$links_cat['cylinders'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/cylinders.png"/></a>
 	                <a href="<?php if(!empty($links_cat['other'])) { echo base_url().'products/category/'.$links_cat['other'][0]->category_id; } ?>"><img src="<?=base_url()?>images/categories/misc.png"/></a>
 	            </div>
 			</div>
@@ -58,7 +58,7 @@
 <!--	        	--><?php //require_once('_new_info.php'); ?>
 	        </div>
 		</div>
-        <?php require_once('_main_partners.php'); ?>        
+        <?php require_once('_main_partners.php'); ?>
 		<div style="clear:both;">&nbsp;</div>
 	</div>
 	<?php require_once('_footer.php'); ?>
